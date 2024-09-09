230c511d0c3b86ebdd9003c222580e9a52a06c9e
kareem-abdul <67004443+kareem-abdul@users.noreply.github.com>
2024-09-09 15:59:04 +0530

    refactor: removes emitter.all and emitter.any functions

--- src/emitter/index.js
+++ src/emitter/index.js
@@ -16,34 +16,6 @@
 
 const { EventEmitter } = require('events')
 
-const emitter = new EventEmitter()
-
-emitter.all = function (events, callback) {
-  const eventList = events.slice(0)
-
-  function onEvent (event) {
-    emitter.on(events[event], function () {
-      eventList.splice(eventList.indexOf(events[event]), 1)
-
-      if (eventList.length === 0) {
-        callback()
-      }
-    })
-  }
-
-  for (const ev in events) {
-    if (Object.hasOwn(events, ev)) {
-      onEvent(ev)
-    }
-  }
-}
-
-emitter.any = function (events, callback) {
-  function onEvent (event) {
-    emitter.on(events[event], function () {
-      if (events !== null) {
-        callback()
-      }
 
 const events = /** @type {const} */ ({
     FAQ_UPDATED: 'faq:update',
