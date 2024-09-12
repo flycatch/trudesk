3008d2f8d7fe4b9f34a3514ee970654f9c5d40fd
kareem-abdul <67004443+kareem-abdul@users.noreply.github.com>
2024-09-12 19:09:56 +0530

    fix(faq): resolves the issue were deleting faq didnot delete it from elasticsearch

--- src/models/faq.js
+++ src/models/faq.js
@@ -1,4 +1,5 @@
 const { emitter, events } = require("@/emitter");
+const logger = require("@/logger");
 const mongoose = require("mongoose");
 
 const COLLECTION = 'faq'
@@ -35,8 +36,33 @@ FaqSchema.post('save', function (doc, next) {
     return next()
 })
 
-FaqSchema.post('deleteOne', function(doc, next) {
+FaqSchema.pre('deleteOne', { query: true }, async function(next) {
+  const id = this.getFilter()["_id"]
+  if (id === undefined) {
+    try {
+      const faq = await Faq.findOne(this.getFilter())
+      this.faq = faq
+      next()
+    } catch (err) {
+      next(err)
+    }
+  }
+  this.id = id
+  next()
+})
+
+FaqSchema.post('deleteOne', { document: true }, function(doc, next) {
     emitter.emit(events.FAQ_DELETED, doc._id)
+    next()
+})
+
+FaqSchema.post('deleteOne', { query: true }, function(doc, next) {
+    const id = this.id ?? this.faq?._id
+    if (id === undefined) {
+        logger.warn("Not able to determin FAQ id. Deleted FAQ will leave inconsistant elastic search data")
+        next()
+    }
+    emitter.emit(events.FAQ_DELETED, id)
     next()
 })
 
@@ -68,6 +94,19 @@ const statics = {
     async getFaqById(_id) {
         return Faq.findOne({ _id }).exec()
     },
+
+    /**
+     * Deletes FAQ by id.
+     *
+     * Note: The delete of faq should trigger the post delete middleware
+     * which requires the id or the document that was deleted
+     *
+     * @param {string} _id - mongoose object id
+     * @returns {Promise.<void>}
+     */
+    async deleteFaq(_id) {
+        await Faq.deleteOne({ _id }).exec()
+    }
 }
 
 FaqSchema.statics = statics;
