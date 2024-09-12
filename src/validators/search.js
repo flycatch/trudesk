b5f1a7b4ca421d7d85375c377c663f72653b5342
kareem-abdul <67004443+kareem-abdul@users.noreply.github.com>
2024-09-12 08:58:23 +0530

    feat(semanticsearch): updates API validation

--- src/validators/search.js
+++ src/validators/search.js
@@ -1,7 +1,7 @@
 const Joi = require("joi");
 
 const SearchRequest = Joi.object({
-  limit: Joi.number().default(20).required(),
+  limit: Joi.number().default(20).min(1).max(50).required(),
   query: Joi.string().max(500, 'utf-8').required(),
 }).required()
 
