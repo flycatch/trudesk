3b661a0f7eb433616cd3ff622d9703fccdbcba5d
kareem-abdul <67004443+kareem-abdul@users.noreply.github.com>
2024-09-13 15:49:00 +0530

    fix(semanticsearch): makes limit,score optional

--- src/validators/search.js
+++ src/validators/search.js
@@ -1,8 +1,8 @@
 const Joi = require("joi");
 
 const SearchRequest = Joi.object({
-  limit: Joi.number().default(20).min(1).max(50).required(),
-  score: Joi.number().default(0.5).min(0).max(1).required(),
+  limit: Joi.number().default(20).min(1).max(50),
+  score: Joi.number().default(0.5).min(0).max(1),
   query: Joi.string().max(500, 'utf-8').required(),
 }).required()
 
