01241ea17d4968639a1558535d7471c05ebfaac4
kareem-abdul <67004443+kareem-abdul@users.noreply.github.com>
2024-09-13 09:28:07 +0530

    feat: adds setting keys to a cosntants module

---
+++ src/settings/settings-keys.js
@@ -0,0 +1,14 @@
+const SettingKeys = /** @type {const} */ ({
+  ES_ENABLE: 'es:enable',
+  ES_HOST: 'es:host',
+  ES_PORT: 'es:port',
+
+  AI_HOST: 'ai:host',
+  AI_BASIC_TOKEN: 'ai:port',
+  TAGGER_USE_INFERENCE: 'tagger:inference:enable'
+})
+
+
+module.exports = {
+  SettingKeys
+}
