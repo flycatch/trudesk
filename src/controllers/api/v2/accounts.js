7b3b5b3f61924446fb80628529781dd6a750799c
kareem-abdul <67004443+kareem-abdul@users.noreply.github.com>
2024-09-19 17:00:27 +0530

    feat(emai-verify): adds API for sending otp to an email

--- src/controllers/api/v2/accounts.js
+++ src/controllers/api/v2/accounts.js
@@ -22,6 +22,13 @@ const Group = require('../../../models/group')
 const Team = require('../../../models/team')
 const Department = require('../../../models/department')
 const passwordComplexity = require('../../../settings/passwordComplexity')
+const { validate } = require('@/validators')
+const { VerifyEmailSchema } = require('@/validators/accounts')
+const apiUtils = require('../apiUtils')
+const { OtpService } = require('@/services/auth/otp')
+const mailer = require('@/mailer')
+const logger = require('@/logger')
+const { defaults } = require('@/settings/settings-keys')
 
 const accountsApi = {}
 
@@ -508,5 +515,23 @@ accountsApi.updatePassword = async (req, res) => {
     return apiUtil.sendApiError(res, 500, err.message)
   }
 }
+
+/**
+ * @type {import('express').RequestHandler<any, any, { email: string }>}
+ */
+accountsApi.verifyEmail = apiUtils.catchAsync(async (req, res) => {
+  const [body, errors] = validate(VerifyEmailSchema, req.body)
+  if (errors) {
+    return apiUtils.sendApiError(res, 400, errors)
+  }
+  const otp = await OtpService.generateOtp(body.email)
+  mailer.sendTemplatedMail({
+    to: body.email,
+    template: 'email-verify-otp',
+    templateProps: { password: otp.password, expiresIn: `${defaults.OTP_EXPIRY / 60} minutes`  }
+  }).catch(err => logger.error('Failed to send email verification mail', err))
+  return apiUtils.sendApiSuccess(res, { message: "Otp succesfully send to email" })
+})
+
 
 module.exports = accountsApi
